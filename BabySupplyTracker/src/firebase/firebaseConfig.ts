b8749a1d9eb497b8712ebda0fd41a571cb4dac64
Yeibin <yeibinkang@gmail.com>
2025-06-04 15:45:12 -0400

    Added Firebase connection codes

---
+++ BabySupplyTracker/src/firebase/firebaseConfig.ts
@@ -0,0 +1,27 @@
+// src/firebase/firebaseConfig.ts
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAh4sPxNrY9z1XgjddclTzDwGWiwPJ-6kE",
+
+  authDomain: "babysupplytracker.firebaseapp.com",
+
+  projectId: "babysupplytracker",
+
+  storageBucket: "babysupplytracker.firebasestorage.app",
+
+  messagingSenderId: "431265839663",
+
+  appId: "1:431265839663:web:02e959a06a0b5dab4ee4da",
+
+  measurementId: "G-81X84NNR53"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
